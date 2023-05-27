568085b2bbbae4b6333e7093fce8b7398d44b048
Farzad Foroozanfar <foruozanfar2000f@gmail.com>
2023-05-27 20:17:45 +0330

    fix

--- src/containers/NotFound/index.jsx
+++ src/containers/NotFound/index.jsx
@@ -3,54 +3,58 @@ import React from "react";
 import { Helmet } from "react-helmet";
 import notfoundGif from "../../assets/images/404.gif";
 import Logo from "../../assets/images/logo.png";
+import Layout from "../../layouts/DashboardLayout";
+
 const NotFoundPage = () => {
   return (
-    <Grid
-      marginTop={1}
-      container
-      spacing={2}
-      direction="row"
-      justifyContent="center"
-      alignItems="center"
-    >
-      <Helmet>
-        <title>خطا 404 - صفحه پیدا نشد | آپارات</title>
-      </Helmet>
+    <Layout>
       <Grid
-        display={"flex"}
+        marginTop={1}
+        container
+        padding={5}
+        direction="row"
         justifyContent="center"
         alignItems="center"
-        item
-        xs={12}
       >
-        <img width={"190px"} src={notfoundGif} alt="aparat" />
-      </Grid>
-      <Grid item style={{ fontSize: "larger", marginTop: "19px" }}>
-        <span style={{ color: "#df0f50" }}>خطای 404 - </span>
-        <span> متاسفانه ویدیو پیدا نشد</span>
-      </Grid>
-      <Grid
-        xs={12}
-        item
-        style={{ textAlign: "center", fontSize: "larger", marginTop: "19px" }}
-      >
+        <Helmet>
+          <title>خطا 404 - صفحه پیدا نشد | آپارات</title>
+        </Helmet>
         <Grid
           display={"flex"}
           justifyContent="center"
           alignItems="center"
           item
-          marginTop={9}
           xs={12}
         >
-          <img
-            src={Logo}
-            alt="aparat"
-            style={{ width: "60px", display: "block" }}
-          />
+          <img width={"190px"} src={notfoundGif} alt="aparat" />
         </Grid>
-        <span style={{ fontSize: '13px' }}> Error 404 - Page Not Found </span>
+        <Grid item style={{ fontSize: "larger", marginTop: "19px" }}>
+          <span style={{ color: "#df0f50" }}>خطای 404 - </span>
+          <span> متاسفانه ویدیو پیدا نشد</span>
+        </Grid>
+        <Grid
+          xs={12}
+          item
+          style={{ textAlign: "center", fontSize: "larger", marginTop: "19px" }}
+        >
+          <Grid
+            display={"flex"}
+            justifyContent="center"
+            alignItems="center"
+            item
+            marginTop={9}
+            xs={12}
+          >
+            <img
+              src={Logo}
+              alt="aparat"
+              style={{ width: "60px", display: "block" }}
+            />
+          </Grid>
+          <span style={{ fontSize: "13px" }}> Error 404 - Page Not Found </span>
+        </Grid>
       </Grid>
-    </Grid>
+    </Layout>
   );
 };
 
