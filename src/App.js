375c8a93bad50a2e89d4f18c5de5d0f6214dcd09
Farzad Foroozanfar <foruozanfar2000f@gmail.com>
2023-05-27 20:17:05 +0330

    add Upload route

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from "./containers/Home";
 import Login from "./containers/Login";
 import NotFoundPage from "./containers/NotFound";
 import Dashboard from "./containers/Dashboard";
+import UploadPage from "./containers/Upload";
 
 export default function App() {
   return (
@@ -11,7 +12,7 @@ export default function App() {
           <Route index element={<Home />} />
           <Route path="login" element={<Login />} />
           <Route path="dashboard" element={<Dashboard />} />
-
+          <Route path="upload" element={<UploadPage />} />
           <Route path="*" element={<NotFoundPage/>}/>
       </Routes>
     </BrowserRouter>
