901390801beb4410dd439a2c13b4b57843ce77b2
Farzad Foroozanfar <foruozanfar2000f@gmail.com>
2023-05-27 20:18:32 +0330

    add content wrapper

--- src/layouts/DashboardLayout/index.jsx
+++ src/layouts/DashboardLayout/index.jsx
@@ -10,20 +10,43 @@ const StyledDashboardWrapper = styled.div`
   & .sidebarWrapper {
     z-index: 0;
   }
+
+  & .uploadPageContainer {
+    background-color: #fbfbfc;
+    height: calc(100vh - 55px) !important;
+  }
 `;
 
-const DashboardLayout = ({ children }) => {
+const DashboardLayout = ({ children, showSidebar = true, uploadPage = false }) => {
+  const classname = uploadPage ? 'uploadPageContainer' : '';
+
   return (
     <StyledDashboardWrapper>
       <Navbar />
-      <Grid container >
-        <Grid item className="sidebarWrapper">
-          <Sidebar />
-        </Grid>
-        <Grid item>{children}</Grid>
+      <Grid container>
+        {showSidebar && (
+          <>
+            <Grid item xs={2} className="sidebarWrapper">
+              <Sidebar />
+            </Grid>
+            <Grid xs={12} md={10} item>
+              {children}
+            </Grid>
+          </>
+        )}
+        {!showSidebar && (
+          <Grid xs={12} md={12}item className={classname}>
+            {children}
+          </Grid>
+        )}
+        {showSidebar && (
+          <Grid xs={12} item className={classname}>
+            {children}
+          </Grid>
+        )}
       </Grid>
     </StyledDashboardWrapper>
-  )
-}
+  );
+};
 
-export default DashboardLayout
+export default DashboardLayout;
