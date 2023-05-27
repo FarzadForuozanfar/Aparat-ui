a1c51c2d546672f7622b3eebea6351e5f049e1e8
Farzad Foroozanfar <foruozanfar2000f@gmail.com>
2023-05-27 20:18:09 +0330

    create upload page

---
+++ src/containers/Upload/index.jsx
@@ -0,0 +1,71 @@
+import { Helmet } from "react-helmet";
+import Layout from "../../layouts/DashboardLayout";
+import { Grid, Typography, Button } from "@mui/material";
+import styled from "styled-components";
+// import { UploadFile as UploadIcon } from "@mui/icons-material";
+// import { FileDrop } from "react-file-drop";
+import UploadLogo from "../../assets/images/upload-light.svg";
+
+const UploadWrapper = styled(Grid)`
+  max-width: 800px;
+  margin: auto;
+  padding: 0px 8px;
+
+  & .fileDropWrapper{
+    margin:50px 0px;
+    padding: 20px;
+    border-radius: 5px;
+    box-shadow: 0px 0px 10px 2.5px #edededed;
+    background: #fff;
+  }
+
+  & .fileDrop{
+    text-align: center;
+    border: 3px dotted #ccc;
+    padding: 20px 50px;
+  }
+  
+  & h5{
+    color: #444;
+    margin:15px 0px;
+  }
+
+  & h6{
+    font-size: 13px;
+    color: #999 !important;
+    margin:20px 0px;
+  }
+
+  & button{
+    margin:15px 0px;
+    border-radius: 45px;
+    font-size: 17px;
+    padding: 10px 20px;
+  }
+`;
+
+export function UploadPage() {
+  return (
+    <Layout uploadPage={true} showSidebar={false}>
+      <Helmet>
+        <title>بارگذاری ویدیو</title>
+        <meta name="description" content="بارگذاری ویدیو" />
+      </Helmet>
+
+      <UploadWrapper container>
+        <Grid item xs={12}>
+          <div className="fileDropWrapper">
+            <div className="fileDrop">
+              <img src={UploadLogo} alt="aparat|upload" />
+              <Typography variant="h5">بارگذاری ویدیو</Typography>
+              <Typography variant="h6">فایل های خود را اینجا بکشید و یا فایل خود را با کلیک انتخاب کنید</Typography>
+              <Button variant="contained">انتخاب ویدیو</Button>
+            </div>
+          </div>
+        </Grid>
+      </UploadWrapper>
+    </Layout>
+  );
+}
+
+export default UploadPage;
